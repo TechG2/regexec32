b920a7f3f4e52cd2b0857675e46deccc72ffd4f2
TechG <133663470+TechG2@users.noreply.github.com>
2024-05-08 22:57:14 +0200

    commit

--- regexec32/main.cpp
+++ regexec32/main.cpp
@@ -13,13 +13,26 @@ int main(int argc, char* argv[]) {
     freopen_s(&fp, "CONOUT$", "w", stdout);
     freopen_s(&fp, "CONOUT$", "w", stderr);
 
+    // converter
+    const size_t newsize = strlen(argv[1]) + 1;
+    wchar_t* wcstring = new wchar_t[newsize];
+    size_t convertedChars = 0;
+    mbstowcs_s(&convertedChars, wcstring, newsize, argv[1], _TRUNCATE);
+
+    // Create wstring for message
+    std::wstring message = L"The module \"";
+    message += std::wstring(wcstring);
+    message += L"\" has been loaded, but entry point DllRegisterServer was not found.\n\nMake sure that \"";
+    message += std::wstring(wcstring);
+    message += L"\" is a valid OCX or file, then try again.";
+
     // Dll Load + execute
     HMODULE hModule = LoadLibraryA(argv[1]);
     if (hModule == NULL) {
         return 1;
     }
 
-    MessageBox(NULL, L"test", L"Error", MB_ICONERROR | MB_OK);
+    MessageBoxW(NULL, message.c_str(), L"RegExec32", MB_ICONERROR | MB_OK);
 
     // Del dll
     FreeLibrary(hModule);
